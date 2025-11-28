ecbe0adcf6e324be56b3849c2c8c82feb0ebf6be
Meklit Tadie <meklittadie4@gmail.com>
2025-11-28 21:06:47 +0300

    lab2 in memory

---
+++ PizzaStore-InMemory/Data/pizzaDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+namespace PizzaStore.Data
+{
+    public class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+
+        public DbSet<Pizza> Pizzas => Set<Pizza>();
+    }
+}
